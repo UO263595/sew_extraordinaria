62232369b670214646f3a91880854cfa29bd9510
UO263595 <56385094+UO263595@users.noreply.github.com>
2021-06-05 12:27:24 +0200

    Update cargar-cartelera.js

--- cargar-cartelera.js
+++ cargar-cartelera.js
@@ -22,7 +22,7 @@ class ArchivoXML {
 				var totalPeliculas = $('pelicula', datos).length;// cuenta el número de películas
 				console.log(totalPeliculas);
 				for (let i=0; i<totalPeliculas; i++) {
-					console.log($('pelicula', datos).get(1).attr("nombre"));
+					console.log($('pelicula', datos).get(1));
 					console.log("entra en bucle");
 				}	
 				console.log("sale del bucle");
