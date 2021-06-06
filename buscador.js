c543287f20c5fea392ca02ad5036c4e3f9a599b8
UO263595 <56385094+UO263595@users.noreply.github.com>
2021-06-06 15:16:50 +0200

    Add files via upload

---
+++ buscador.js
@@ -0,0 +1,50 @@
+"use strict";
+class BuscadorNoticias {
+	constructor() {
+		this.apikey = "ac89e850d0fce1d544cba3aa5f886995";
+		this.idioma = "&lang=es";
+		this.orden = "&sortBy=publishedAt"; // ordena las noticias por fecha de publicación
+	}
+	cargarDatos(){
+		$.ajax({
+			dataType: "json",
+			url: this.url,
+			method: 'GET',
+			success: function(datos) {
+				var stringArticulos = "";
+				for (var i = 0; i < datos.articles.length; i+=1) {
+					stringArticulos += "<h3>" + datos.articles[i].title + " - " + datos.articles[i].source.name + "</h3>";
+					stringArticulos += "<img src='" + datos.articles[i].image + "'/>";
+					stringArticulos += "<p>" + datos.articles[i].description + "</p>";
+					stringArticulos += "<a href='" + datos.articles[i].url + "'>Ver noticia</a>";
+				}
+				
+				$("div").html(stringArticulos);
+				$("h4").html("Total resultados: " + datos.totalArticles);
+			},
+			error:function(){
+				$("h3").html("¡Tenemos problemas! No puedo obtener JSON de <a href='https://gnews.io/'>GNews</a>"); 
+
+			}
+		});
+	}
+	crearElemento(tipoElemento, texto, insertarDespuesDe) {
+		var elemento = document.createElement(tipoElemento); 
+		elemento.innerHTML = texto;
+		$(insertarDespuesDe).after(elemento);
+	}
+	buscar() {
+		$("div").remove();
+		$("h3").remove();
+		$("h4").remove();
+		var busqueda = $("#inputBusqueda").val();
+		// https://gnews.io/api/v4/search?q=example&token=API-Token
+		this.url = "https://gnews.io/api/v4/search?q=" + busqueda + this.idioma + this.orden + "&token=" + this.apikey;
+		console.log("El valor actual de la url es " + this.url);
+		this.crearElemento("h4","","#bBuscar");
+		this.crearElemento("div","","#bBuscar");
+		this.cargarDatos();
+	}
+}
+
+var buscadorNoticias = new BuscadorNoticias();
