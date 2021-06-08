303b98a266bb5a6294e547c62f50fe9409dacd33
UO263595 <56385094+UO263595@users.noreply.github.com>
2021-06-08 12:24:03 +0200

    Add files via upload

--- tiempo.js
+++ tiempo.js
@@ -6,12 +6,34 @@
 class InfoTiempo {
     constructor() {
         navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this), this.verErrores.bind(this));
-    }
+		this.apikey = "187343ea60ec406387d3986333ad19f1";
+		this.idioma = "&lang=es";
+	}
+	
+	cargarDatos(mensajeTiempo) {
+		$.ajax({
+			dataType: "json",
+			url: this.url,
+			method: 'GET',
+			success: function(datos) {
+				var stringDatos = "";
+				stringDatos += "<h3>"+mensajeTiempo+"</h3>";
+				stringDatos += "<p>Dirección del viento: "+datos.data[0].wind_cdir+"</p>";
+
+				document.getElementById('sectionTiempo').innerHTML = stringDatos;
+			},
+			error:function() {
+				$("h3").html("¡Tenemos problemas! No puedo obtener JSON de <a href='https://mediastack.com/'>Mediastack</a>"); 
+
+			}
+		});
+	}
 	
 	// Consigue la posición actual del usuario
     getPosicion(posicion) {
         this.longitud = posicion.coords.longitude; 
         this.latitud = posicion.coords.latitude;  
+		this.mensaje = "Se ha realizado correctamente la petición de geolocalización";	
     }
 	
 	// Comprueba si hay algún error
@@ -57,6 +79,14 @@ class InfoTiempo {
 			return "NO DISPONIBLE";
 		}
 		return param;
+	}
+	
+	// Muestra los datos del tiempo actual
+	tiempoActual() {
+		// https://api.weatherbit.io/v2.0/current
+		this.url = "https://api.weatherbit.io/v2.0/current?lat=" + this.getLatitud() + "&lon=" + this.getLongitud() + "&key=" + this.apikey;
+		console.log("El valor actual de la url es " + this.url);
+		this.cargarDatos("Tiempo actual");
 	}
 }
 
