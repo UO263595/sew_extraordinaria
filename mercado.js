e0070c25defca4b3eac1767ae40268c0f11f7267
UO263595 <56385094+UO263595@users.noreply.github.com>
2021-06-10 09:57:04 +0200

    Add files via upload

--- mercado.js
+++ mercado.js
@@ -7,6 +7,10 @@
 class BuscadorMercado {
 	// Constructor de la clase
 	constructor() {
+		this.fechaActual = new Date();
+		console.log(this.fechaActual.toLocaleDateString());
+		console.log();
+		
 		this.apikey = "04e81f6ca5cd24036c4e875f36e73d99";
 		this.orden = "&sort=ASC"; // ordena los por fecha
 	}
